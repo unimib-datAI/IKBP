0c29f5af30ef6dc744fac984855f2be78050d220
Blowdire <agazzi.ruben99@gmail.com>
2024-07-09 16:51:31 +0200

    removed mongo password

--- documents/src/loaders/mongo.js
+++ documents/src/loaders/mongo.js
@@ -2,13 +2,11 @@ import mongoose from "mongoose";
 
 export const mongoLoader = async () => {
   try {
-    console.log("Setup mongodb...", process.env.MONGO);
+    console.log("Setup mongodb...");
     await mongoose.connect(process.env.MONGO, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
-    
   } catch (err) {
     throw new Error("Couldn't not connecto to DB.");
   }
