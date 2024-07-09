f180bb35786fabfca76670631da44e93562bb6c7
Blowdire <agazzi.ruben99@gmail.com>
2024-07-09 15:00:33 +0200

    other

--- documents/src/loaders/index.js
+++ documents/src/loaders/index.js
@@ -2,7 +2,7 @@ import { expressLoader } from "./express";
 import { mongoLoader } from "./mongo";
 
 export const startServer = async (callback) => {
-  const PORT = process.env.PORT;
+  const PORT = process.env.DOCS_PORT;
   // setup express routes
   const app = expressLoader();
   // setup mongodb
@@ -12,4 +12,4 @@ export const startServer = async (callback) => {
   const server = app.listen(PORT, () => callback({ PORT }));
 
   return server;
-}
+};
